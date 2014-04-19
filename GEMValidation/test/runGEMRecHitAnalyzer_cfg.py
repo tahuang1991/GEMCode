b7e8138a8584778c8cb62e10e1e9adc34c0e8196
Sven Dildick <sven.dildick@cern.ch>
2014-04-19 03:33:32 -0500

    use helper functions

--- GEMValidation/test/runGEMRecHitAnalyzer_cfg.py
+++ GEMValidation/test/runGEMRecHitAnalyzer_cfg.py
@@ -21,23 +21,14 @@ from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2019', '')
 
 # the analyzer configuration
-from GEMCode.GEMValidation.simTrackMatching_cfi import SimTrackMatching
+from GEMCode.GEMValidation.simTrackMatching_cfi import *
 process.GEMRecHitAnalyzer = cms.EDAnalyzer("GEMRecHitAnalyzer",
     simTrackMatching = SimTrackMatching
 )
-process.GEMRecHitAnalyzer.simTrackMatching.gemStripDigi.input = ""
-process.GEMRecHitAnalyzer.simTrackMatching.gemPadDigi.input = ""
-process.GEMRecHitAnalyzer.simTrackMatching.gemCoPadDigi.input = ""
-process.GEMRecHitAnalyzer.simTrackMatching.cscStripDigi.input = ""
-process.GEMRecHitAnalyzer.simTrackMatching.cscWireDigi.input = ""
-process.GEMRecHitAnalyzer.simTrackMatching.cscCLCT.input = ""
-process.GEMRecHitAnalyzer.simTrackMatching.cscALCT.input = ""
-process.GEMRecHitAnalyzer.simTrackMatching.cscLCT.input = ""
-process.GEMRecHitAnalyzer.simTrackMatching.cscMPLCT.input = ""
-process.GEMRecHitAnalyzer.simTrackMatching.tfTrack.input = ""
-process.GEMRecHitAnalyzer.simTrackMatching.tfCand.input = ""
-process.GEMRecHitAnalyzer.simTrackMatching.gmtCand.input = ""
-process.GEMRecHitAnalyzer.simTrackMatching.l1Extra.input = ""
+## simhits and rechits
+process.GEMRecHitAnalyzer = noDigiCollections(process.GEMRecHitAnalyzer)
+process.GEMRecHitAnalyzer = noL1Collections(process.GEMRecHitAnalyzer)
+process.GEMRecHitAnalyzer = noTrackCollections(process.GEMRecHitAnalyzer)
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
