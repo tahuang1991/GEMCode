acb01bc3d7c02afc4607f8b50f91507bd1b5784e
Sven Dildick <sven.dildick@cern.ch>
2016-04-30 13:50:08 -0500

    Update randomizeMuonGun.py

--- GEMValidation/python/randomizeMuonGun.py
+++ GEMValidation/python/randomizeMuonGun.py
@@ -7,6 +7,19 @@ def randomizeMuonGun(process):
         pdgId = cms.vint32(13, -13)
     )
 
+    ## filter sequence
+    process.gen_mu_select = cms.Sequence(process.genMuons)
+    process.generation_step += process.gen_mu_select
+    process.simulation_step += process.gen_mu_select
+    return process
+    
+def randomizeMuonGunGEM(process):
+    ## filter for gen level
+    process.genMuons = cms.EDFilter("PdgIdCandViewSelector",
+        src = cms.InputTag("genParticles"),
+        pdgId = cms.vint32(13, -13)
+    )
+
     ## filter for sim level
     process.genMuonsGEM = cms.EDFilter("CandViewSelector",
         src = cms.InputTag("genMuons"),
