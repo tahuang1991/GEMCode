70b2bce1c8cf9b935d941114cdfa2e002e3ac0ce
Sven Dildick <sven.dildick@cern.ch>
2017-04-25 23:04:06 -0500

    config for pu200

---
+++ GEMValidation/python/DigiFullPU_2023tiltedPU200.py
@@ -0,0 +1,108 @@
+# Auto generated configuration file
+# using:
+# Revision: 1.19
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v
+# with command line options: step2 --conditions auto:phase2_realistic -n 10 --era Phase2C2_timing --eventcontent FEVTDEBUGHLT -s DIGI:pdigi_valid,L1 --datatier GEN-SIM-DIGI --pileup AVE_200_BX_25ns --geometry Extended2023D4 --python DigiFullPU_2023tiltedPU200.py --no_exec --filein file:step1.root --fileout file:step2.root
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('L1',eras.Phase2C2_timing)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mix_POISSON_average_cfi')
+process.load('Configuration.Geometry.GeometryExtended2023D4Reco_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
+    fileNames = cms.untracked.vstring('file:step1.root'),
+    inputCommands = cms.untracked.vstring('keep *',
+        'drop *_genParticles_*_*',
+        'drop *_genParticlesForJets_*_*',
+        'drop *_kt4GenJets_*_*',
+        'drop *_kt6GenJets_*_*',
+        'drop *_iterativeCone5GenJets_*_*',
+        'drop *_ak4GenJets_*_*',
+        'drop *_ak7GenJets_*_*',
+        'drop *_ak8GenJets_*_*',
+        'drop *_ak4GenJetsNoNu_*_*',
+        'drop *_ak8GenJetsNoNu_*_*',
+        'drop *_genCandidatesForMET_*_*',
+        'drop *_genParticlesForMETAllVisible_*_*',
+        'drop *_genMetCalo_*_*',
+        'drop *_genMetCaloAndNonPrompt_*_*',
+        'drop *_genMetTrue_*_*',
+        'drop *_genMetIC5GenJs_*_*'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('step2 nevts:10'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.FEVTDEBUGHLToutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM-DIGI'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(10485760),
+    fileName = cms.untracked.string('file:step2.root'),
+    outputCommands = process.FEVTDEBUGHLTEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+process.mix.input.nbPileupEvents.averageNumber = cms.double(200.000000)
+process.mix.bunchspace = cms.int32(25)
+process.mix.minBunch = cms.int32(-12)
+process.mix.maxBunch = cms.int32(3)
+process.mix.digitizers = cms.PSet(process.theDigitizersValid)
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:phase2_realistic', '')
+
+# Path and EndPath definitions
+process.digitisation_step = cms.Path(process.pdigi_valid)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.FEVTDEBUGHLToutput_step = cms.EndPath(process.FEVTDEBUGHLToutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.digitisation_step,process.L1simulation_step,process.endjob_step,process.FEVTDEBUGHLToutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
+
+from GEMCode.GEMValidation.addPileup import addPileup
+process = addPileup(process)
---
+++ GEMValidation/python/fileNamesPU_MinBias_14_82X_Phase2.py
@@ -0,0 +1,66 @@
+import FWCore.ParameterSet.Config as cms
+import os
+
+def getFiles(my_dir):
+    theInputFiles = []
+    if not os.path.isdir(my_dir):
+        print "ERROR: This is not a valid directory: ", my_dir
+    for file in os.listdir(my_dir):
+        if file.endswith('root'):
+            if os.path.getsize(my_dir + file) > 20000000:
+                theInputFiles.extend(['file:' + my_dir + file])
+    return theInputFiles
+
+fileNamesPU = cms.untracked.vstring()
+
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170228/GENSIM_10M_v1/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170228/GENSIM_10M_v2/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170228/GENSIM_10M_v3/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170228/GENSIM_10M_v4/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170228/GENSIM_10M_v5/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170228/GENSIM_10M_v6/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170228/GENSIM_10M_v7/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170228/GENSIM_10M_v8/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170228/GENSIM_10M_v9/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170228/GENSIM_10M_v10/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170228/GENSIM_10M_v11/"))
+
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170404_10M_v1/GENSIM_10M_v1/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170404_10M_v1/GENSIM_10M_v10/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170404_10M_v1/GENSIM_10M_v2/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170404_10M_v1/GENSIM_10M_v3/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170404_10M_v1/GENSIM_10M_v4/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170404_10M_v1/GENSIM_10M_v5/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170404_10M_v1/GENSIM_10M_v6/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170404_10M_v1/GENSIM_10M_v7/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170404_10M_v1/GENSIM_10M_v8/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170404_10M_v1/GENSIM_10M_v9/"))
+
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170407_10M_v1/GENSIM_10M_v1/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170407_10M_v1/GENSIM_10M_v10/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170407_10M_v1/GENSIM_10M_v2/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170407_10M_v1/GENSIM_10M_v3/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170407_10M_v1/GENSIM_10M_v4/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170407_10M_v1/GENSIM_10M_v5/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170407_10M_v1/GENSIM_10M_v6/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170407_10M_v1/GENSIM_10M_v7/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170407_10M_v1/GENSIM_10M_v8/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170407_10M_v1/GENSIM_10M_v9/"))
+
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v0/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v1/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v10/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v11/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v12/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v13/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v14/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v15/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v16/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v2/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v3/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v4/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v5/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v6/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v7/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v8/"))
+fileNamesPU.extend(getFiles("/eos/uscms/store/user/lpcgem/ME0TDRStudies/MinBias_20170406_10M_v1_Tao/GENSIM_10M_v9/"))
