0764b05d6223c8657e1c494c988c8e043c478a48
Sven Dildick <sven.dildick@cern.ch>
2017-07-19 01:12:34 -0500

    use official minbias files

--- GEMValidation/python/fileNamesPU_MinBias_14TeV_91X_Phase2.py
+++ GEMValidation/python/fileNamesPU_MinBias_14TeV_91X_Phase2.py
@@ -1,8 +1,16 @@
 import FWCore.ParameterSet.Config as cms
+import os
 
 fileNamesPU = cms.untracked.vstring()
 
-f = open('fileNamesPU_MinBias_14TeV_91X_Phase2.txt')
-for line in f:
-    fileNamesPU.extend((line))
-  
+## absolute path to txt file
+path = os.getenv( "CMSSW_BASE" ) + "/src/GEMCode/GEMValidation/python/"
+
+## load the official MinBias files for Muon TDR production
+f = open(path + 'fileNamesPU_MinBias_14TeV_91X_Phase2.txt')
+pu_files = f.read().split('\n')
+f.close()
+
+## add them to a list
+pu_files = filter(lambda x: x.endswith('.root'),  pu_files)
+fileNamesPU.extend(pu_files)
